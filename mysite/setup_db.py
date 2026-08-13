14ef55a93622979ede330e67a9ba5a69ec61f53f
Your Name <you@example.com>
2026-08-13 23:31:16 +0000

    sync 2026-08-13_23-31-16

--- mysite/setup_db.py
+++ mysite/setup_db.py
@@ -1,30 +1,33 @@
 import sqlite3
-
 conn = sqlite3.connect("books.db")
-
 cursor = conn.cursor()
-
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     author TEXT,
-    year INTEGER,
-    part INTEGER,
-    current_page INTEGER,
-    total_pages INTEGER,
-    words_per_minute INTEGER,
-
-    drive_url TEXT,
-    book_url TEXT,
-    pdf_url TEXT,
-
-    category TEXT,
-    notes TEXT
+    year TEXT,
+    category TEXT
 )
 """)
-
+books = [
+    ("french_revolution", "tocqueville", "1856", ""),
+    ("ten days", "john reed", "1919", ""),
+    ("too_big_to_fail", "sorkin", "2009", ""),
+    ("jp_morgan", "martin", "2022", ""),
+    ("common sense", "paine", "1775", ""),
+    ("finance", "libby", "idk..?", ""),
+    ("grant", "grant", "1885", ""),
+    ("state and revolution", "lenin", "1917", "bolshevism"),
+    ("imperialism", "lenin", "1917", "bolshevism"),
+    ("ochrana", "", "1917", "imperialists"),
+    ("medici money", "", "2006", "imperialists"),
+    ("practical idealism", "kalergi", "1925", "imperialists")
+]
+cursor.executemany("""
+INSERT INTO books (title, author, year, category)
+VALUES (?, ?, ?, ?)
+""", books)
 conn.commit()
 conn.close()
-
-print("Database created!")
+print("Database created and books added!")
